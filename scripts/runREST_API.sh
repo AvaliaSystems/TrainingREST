33051d7bc1dfc615b6ebce13030263fae43e492f
pabloheigvd <pabloheigvd@gmail.com>
2020-11-11 12:03:29 +0100

    dockerisation impl ok

--- scripts/runREST_API.sh
+++ scripts/runREST_API.sh
@@ -2,5 +2,8 @@
 echo "============================"
 echo "=== Starting up REST API ==="
 echo "============================"
+cd ../fruits-impl
+mvn clean package # generate jar to copy
 cd ..
+docker-compose down -v
 docker-compose up
