352cb26fffd00a1c6b209d5affa4a8bf2a38241d
pabloheigvd <pabloheigvd@gmail.com>
2020-11-19 21:22:13 +0100

    [WIP] api endpoints still fruits somehow

--- scripts/runREST_API.sh
+++ scripts/runREST_API.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+
+echo "========================="
+echo "=== Building REST API ==="
+echo "========================="
+cd ../gamification-impl
+mvn clean package # generate jar to copy
+cd target
+mv gamification-impl-1.0.0.jar ../../docker/api/api.jar
+cd ../..
+docker-compose down -v
 echo "============================"
 echo "=== Starting up REST API ==="
 echo "============================"
-cd ../fruits-impl
-mvn clean package # generate jar to copy
-cd target
-mv fruits-impl-1.0.0.jar ../../docker/api/api.jar
-cd ../..
-docker-compose down -v
 docker-compose up &
