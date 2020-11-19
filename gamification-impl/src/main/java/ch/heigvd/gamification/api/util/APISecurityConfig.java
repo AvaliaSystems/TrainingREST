ba2ee3d96c1f4fddfcc7dccbbd645736c6a993e9
Sacha Perdrizat <sacha.perdrizat@gmail.com>
2020-11-20 00:00:33 +0100

    AuthFilter with SpringSecurity

---
+++ gamification-impl/src/main/java/ch/heigvd/gamification/api/util/APISecurityConfig.java
@@ -0,0 +1,48 @@
+package ch.heigvd.gamification.api.util;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+
+@Configuration
+@EnableWebSecurity
+@Order(1)
+public class APISecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("X-API-KEY")
+    private String principalRequestHeader;
+
+    @Value("password1234")
+    private String principalRequestValue;
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        APIKeyAuthFilter filter = new APIKeyAuthFilter(principalRequestHeader);
+        filter.setAuthenticationManager(new AuthenticationManager() {
+
+            @Override
+            public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+                String principal = (String) authentication.getPrincipal();
+                if (!principalRequestValue.equals(principal))
+                {
+                    throw new BadCredentialsException("The API key was not found or not the expected value.");
+                }
+                authentication.setAuthenticated(true);
+                return authentication;
+            }
+        });
+        httpSecurity.
+                antMatcher("/applications/**").
+                csrf().disable().
+                sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).
+                and().addFilter(filter).authorizeRequests().anyRequest().authenticated();
+    }
+
+}
