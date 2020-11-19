35521c0983501f453d16bbdba465e07b3f4c7a6e
Basile <basile.botebol@gmail.com>
2020-11-19 17:52:15 +0100

    changing datasource url

--- gamification-impl/src/main/java/ch/heig/gamification/api/endpoints/ApplicationsApiController.java
+++ gamification-impl/src/main/java/ch/heig/gamification/api/endpoints/ApplicationsApiController.java
@@ -10,7 +10,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import java.util.UUID;
 import javax.validation.Valid;
 
