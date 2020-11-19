47b11fe7ce731f9deb7df7259559115f3d5a1a82
vitorva <vitor.vazafonso@gmail.com>
2020-11-19 14:57:24 +0100

    Implements application structure

---
+++ gamification-impl/src/main/java/ch/heigvd/amt/gamification/repositories/ApplicationRepository.java
@@ -0,0 +1,8 @@
+package ch.heigvd.amt.gamification.repositories;
+
+import ch.heigvd.amt.gamification.entities.ApplicationEntity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ApplicationRepository extends CrudRepository<ApplicationEntity, Long> {
+
+}
