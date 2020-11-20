c31d8d214b2ad9875707bc9daf9dfee0af5d5a15
Kleeer <delhomme.claire.ts7@gmail.com>
2020-11-20 10:09:51 +0100

    applications.feature // pb fixed

--- gamification-specs/src/test/java/ch/heigvd/amt/gamification/api/spec/steps/BasicSteps.java
+++ gamification-specs/src/test/java/ch/heigvd/amt/gamification/api/spec/steps/BasicSteps.java
@@ -3,6 +3,7 @@ package ch.heigvd.amt.gamification.api.spec.steps;
 import ch.heigvd.amt.gamification.ApiException;
 import ch.heigvd.amt.gamification.ApiResponse;
 import ch.heigvd.amt.gamification.api.DefaultApi;
+import ch.heigvd.amt.gamification.api.dto.Application;
 import ch.heigvd.amt.gamification.api.dto.Badge;
 import ch.heigvd.amt.gamification.api.spec.helpers.Environment;
 import io.cucumber.java.en.And;
@@ -109,6 +110,41 @@ public class BasicSteps {
         lastApiResponse = null;
         lastApiException = apiException;
         lastStatusCode = lastApiException.getCode();
+    }
+
+    // ================= APPLICATIONS ====================================
+    Application application;
+
+    @Given("there is an Applications server")
+    public void there_is_an_applications_server() {
+        assertNotNull(api);
+    }
+
+    @When("I send a GET to the \\/applications endpoint")
+    public void i_send_a_get_to_the_applications_endpoint() {
+        try {
+            lastApiResponse = api.getBadgesWithHttpInfo();
+            processApiResponse(lastApiResponse);
+        } catch (ApiException e) {
+            processApiException(e);
+        }
+    }
+
+    @Given("I have a application payload")
+    public void i_have_a_application_payload() {
+        application = new ch.heigvd.amt.gamification.api.dto.Application()
+                .key("mockAppKey")
+                .name("mockAppnName");
+    }
+
+    @When("I POST the application payload to the \\/applications endpoint")
+    public void i_post_the_application_payload_to_the_applications_endpoint() {
+        try {
+            lastApiResponse = api.registerApplicationWithHttpInfo(application);
+            processApiResponse(lastApiResponse);
+        } catch (ApiException e) {
+            processApiException(e);
+        }
     }
 
 }
