f2273ea91368f6ceef6874e53cb395935c4ffd3e
Kleeer <delhomme.claire.ts7@gmail.com>
2020-12-09 12:45:41 +0100

    try to revert to docker-compose

--- scripts/testREST_API.sh
+++ scripts/testREST_API.sh
@@ -7,9 +7,10 @@ cp -R ../../gamification-specs gamification-specs
 cd gamification-specs
 # https://stackoverflow.com/a/7456028
 #mvn clean package -Dmaven.test.skip=true -Dmaven.test.error.ignore=false # generate jar to copy
-mvn clean package -Dmaven.test.error.ignore=false # generate jar to copy
+mvn clean package -Dmaven.test.skip=true  # generate jar to copy
+#mvn clean package -Dmaven.test.error.ignore=false # generate jar to copy
 cd ../../..
 echo "========================"
 echo "=== Testing REST API ==="
 echo "========================"
-#docker-compose up --build --force-recreate testcucumber
+docker-compose up --build --force-recreate testcucumber
