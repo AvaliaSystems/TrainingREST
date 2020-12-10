0a3bc6fdb876ece79458af7bbd76496a71abf103
Sacha Perdrizat <sacha.perdrizat@gmail.com>
2020-12-10 21:42:01 +0100

    clean eventprocessing

--- gamification-impl/src/main/java/ch/heigvd/gamification/api/services/EventProcessorService.java
+++ gamification-impl/src/main/java/ch/heigvd/gamification/api/services/EventProcessorService.java
@@ -4,6 +4,5 @@ import ch.heigvd.gamification.api.model.Event;
 import ch.heigvd.gamification.api.model.User;
 
 public interface EventProcessorService {
-    public void addBadgetoUser(String username, String apikey);
     public void applyRules(String apikey,Event event);
 }
--- gamification-impl/src/main/java/ch/heigvd/gamification/api/services/EventProcessorServiceImpl.java
+++ gamification-impl/src/main/java/ch/heigvd/gamification/api/services/EventProcessorServiceImpl.java
@@ -3,10 +3,7 @@ package ch.heigvd.gamification.api.services;
 import ch.heigvd.gamification.api.model.Event;
 import ch.heigvd.gamification.api.model.Rule;
 import ch.heigvd.gamification.api.model.RuleThenAction;
-import ch.heigvd.gamification.entities.ApplicationEntity;
-import ch.heigvd.gamification.entities.BadgeEntity;
-import ch.heigvd.gamification.entities.RuleEntity;
-import ch.heigvd.gamification.entities.UserEntity;
+import ch.heigvd.gamification.entities.*;
 import ch.heigvd.gamification.repositories.ApplicationRepository;
 import ch.heigvd.gamification.repositories.BadgeRepository;
 import ch.heigvd.gamification.repositories.RuleRepository;
@@ -34,30 +31,6 @@ public class EventProcessorServiceImpl implements EventProcessorService{
     RuleRepository ruleRepository;
 
     @Override
-    public void addBadgetoUser(String apikey, String username) {
-        List<BadgeEntity> badges = new ArrayList<>();
-        List<BadgeEntity> userBadges = null;
-
-        badgeRepository.findByApplicationEntity_ApiKey(apikey)
-                .forEach(badge -> badges.add(badge));
-
-        UserEntity currentUser = userRepository
-                .findByApplicationEntity_ApiKeyAndUsername(apikey,username)
-                .orElse(null);
-
-
-        if(currentUser == null){
-            currentUser = this.createUser(username,applicationRepository.findByApiKey(apikey));
-        }
-
-        userBadges = currentUser.getBadgeEntitys();
-        userBadges.add(badges.get(0));
-        currentUser.setBadgeEntitys(userBadges);
-
-        userRepository.save(currentUser);
-    }
-
-    @Override
     public void applyRules(String apikey,Event event) {
 
         UserEntity concernedUser = userRepository.findByApplicationEntity_ApiKeyAndUsername(apikey,event.getEventparams().getUsername()).orElse(null);;
@@ -79,6 +52,12 @@ public class EventProcessorServiceImpl implements EventProcessorService{
                 userRepository.save(userEntity);
                 break;
             case "addPoint":
+                userEntity.getPointscaleEntitys().forEach(pointscaleEntity -> {
+                    if (pointscaleEntity.getLabel().equals(ruleEntity.getActionTarget())){
+                        pointscaleEntity.setCounter(pointscaleEntity.getCounter() + 1);
+                    }
+                    userRepository.save(userEntity);
+                });
                 break;
             default:
                 break;
