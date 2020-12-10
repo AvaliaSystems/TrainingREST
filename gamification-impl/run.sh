9a9f44e95dffb68feb3d3e08fa57c011b20708ec
Sacha Perdrizat <sacha.perdrizat@gmail.com>
2020-12-10 21:55:37 +0100

    rm run.sh

--- gamification-impl/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker-compose -f ../docker/topologies/docker-compose.yaml down
-
-docker-compose -f ../docker/topologies/docker-compose.yaml up --detach amtdb amtadminer
-
-mvn spring-boot:run
