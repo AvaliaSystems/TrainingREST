293deb88080db1794aa3d239deb023915b206290
Zaretti <g.zaretti@gmail.com>
2020-12-18 10:05:44 +0100

    before pres

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
-
-cd ../gamification-impl/
+cd ..
+readonly BASE_DIR="$PWD"
+cd gamification-impl/
 mvn install
-rm -f ../docker/images/springboot/apps/gamification-impl-1.0.0.jar
-cp target/gamification-impl-1.0.0.jar ../docker/images/springboot/apps
-cd ../docker
+rm -f "$BASE_DIR"/docker/images/springboot/apps/gamification-impl-1.0.0.jar
+cp target/gamification-impl-1.0.0.jar "$BASE_DIR"/docker/images/springboot/apps
 
-cd ../gamification-specs
-mvn install
-rm -rf ../docker/images/mvnruntest/gamification-specs
-cp -r ./ ../docker/images/mvnruntest/gamification-specs
-cd ../docker
+cd "$BASE_DIR"/gamification-specs
+rm -rf "$BASE_DIR"/docker/images/failsafe/pom.xml
+rm -rf "$BASE_DIR"/docker/images/failsafe/src
+cp -r ./pom.xml "$BASE_DIR"/docker/images/failsafe/pom.xml
+cp -r ./src "$BASE_DIR"/docker/images/failsafe/src
+cd "$BASE_DIR"/docker
