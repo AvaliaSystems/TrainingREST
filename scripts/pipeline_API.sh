0000edd67b2147c2c39c925c5ec03ac1c55dd1c4
pabloheigvd <pabloheigvd@gmail.com>
2020-12-18 11:51:06 +0100

    tty error in pipeline, removing -t

--- scripts/pipeline_API.sh
+++ scripts/pipeline_API.sh
@@ -4,7 +4,7 @@
 #../scripts/wait-for-it.sh -t 70 apirest:8080 --
 
 docker build --tag bashwait ../docker/bashwait
-docker run --network=project_2_web -it bashwait bash -c "./wait-for-it.sh -t 50 apirest:8080 && exit"
+docker run --network=project_2_web -i bashwait bash -c "./wait-for-it.sh -t 50 apirest:8080 && exit"
 
 cd ../gamification-specs
 
